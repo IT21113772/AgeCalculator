dc2b0e6bbb4648c1801be2e3abafa0a9f80b33ae
Menusha <it21113772@my.sliit.lk>
2022-08-18 18:42:04 +0530

    Test commit

---
+++ app/src/main/java/com/example/agecalculator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.agecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtYear;
+    TextView txtMessage;
+    Button btnCalculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtYear = findViewById(R.id.editText);
+        txtMessage = findViewById(R.id.textView);
+    }
+
+    public void calculate(View view) {
+
+        if (!edtYear.getText().toString().isEmpty()) {
+            int birthYear = Integer.parseInt(edtYear.getText().toString());
+            int currentYear = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));
+
+            //txtMessage.setText("You are " + (currentYear - birthYear) + " years old");
+            txtMessage.setText(getResources().getString(R.string.message, (currentYear - birthYear)));
+        } else {
+            //Toast.makeText(this, "Please add a valid year", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getResources().getString(R.string.error1), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
